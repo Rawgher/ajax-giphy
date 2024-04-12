7b05c6a8d2bddb9d216066b86fcb84257cca93dc
Rawgher <rdpender3@gmail.com>
2024-04-12 14:16:19 -0700

    finished project

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("Let's get this party started!");
+const searchInput = document.querySelector('#search-input')
+const searchBtn = document.querySelector('#search-btn');
+const deleteBtn = document.querySelector('#delete-btn');
+const gifDiv = document.querySelector('#gif-div');
+
+async function getGif(term) {
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${term}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    appendGif(response.data);
+}
+
+function appendGif(data) {
+    gifDiv.append(makeNewGifDiv(data.data[0].images.original.url))
+}
+
+function makeNewGifDiv(src) {
+    const newImg = document.createElement('img');
+    newImg.src = src
+    return newImg;
+}
+
+function handleSearch(e) {
+    e.preventDefault();
+    getGif(searchInput.value);
+    searchInput.value = ''
+}
+
+function clearGifs() {
+    gifDiv.innerHTML = '';
+}
+
+searchBtn.addEventListener('click', handleSearch);
+deleteBtn.addEventListener('click', clearGifs);
